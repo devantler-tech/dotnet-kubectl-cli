e489c77beb4af74062c9ef097726c1da6e565581
Nikolai Emil Damm <neq@energinet.dk>
2025-01-25 09:16:43 +0100

    Rename variable 'message' to 'output' for consistency in RunAsync method usage
    Signed-off-by: Nikolai Emil Damm <neq@energinet.dk>

--- Devantler.KubectlCLI.Tests/KubectlTests/RunAsyncTests.cs
+++ Devantler.KubectlCLI.Tests/KubectlTests/RunAsyncTests.cs
@@ -13,11 +13,11 @@ public class RunAsyncTests
   public async Task RunAsync_Version_ReturnsVersion()
   {
     // Act
-    var (exitCode, message) = await Kubectl.RunAsync(["version", "--client"]);
+    var (exitCode, output) = await Kubectl.RunAsync(["version", "--client"]);
 
     // Assert
     Assert.Equal(0, exitCode);
     // Client Version: v1.32.0
-    Assert.Matches(@"^Client Version: v\d+\.\d+\.\d+$", message.Split(Environment.NewLine).First());
+    Assert.Matches(@"^Client Version: v\d+\.\d+\.\d+$", output.Split(Environment.NewLine).First());
   }
 }
